419c27047ff21d41dd051a5c84088bb2a375a526
SoreNNe <71930836+SoreNNe@users.noreply.github.com>
2020-10-10 20:09:11 +0200

    .

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -6,12 +6,13 @@ namespace Open_Lab_02._01
     {
         public double[] Create5Numbers()
         {
-            throw new NotImplementedException();
+            double[] cisla = new double[5];
+            return cisla;
         }
 
         public double GetSecond(double[] numbers)
         {
-            throw new NotImplementedException();
+            return 1;
         }
     }
 }
